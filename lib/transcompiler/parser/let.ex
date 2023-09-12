15d4fa76e18ec94fca84757a3cda4fe1a148867a
Rafael Willians <me@rwillians.com>
2023-09-12 11:17:04 -0300

    Removed old transpiler stuff

--- lib/transcompiler/parser/let.ex
+++
@@ -1,9 +0,0 @@
-defmodule Transcompiler.Parser.Let do
-  @moduledoc false
-
-  @type t :: %Transcompiler.Parser.Let{
-          varname: atom,
-          value: term
-        }
-  defstruct [:varname, :value]
-end
